d962aeb0afcc4f368c0a5e563a89eb6c9daef62f
lilgfc <163637578+lilgfc@users.noreply.github.com>
2025-10-27 03:29:00 +0900

    Initialize kagglecell3.py with imports and settings

---
+++ kagglecell3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import warnings
+warnings.filterwarnings('ignore')
+
+# Set plotting style
+sns.set_style("whitegrid")
+plt.rcParams['figure.figsize'] = (14, 8)
